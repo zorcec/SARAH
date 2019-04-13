f92afdd7ae1390f1634ec16f9daaf5a07c5767a1
Tomislav Zorcec <zorcec@gmail.com>
2019-04-13 20:44:32 +0200

    Making it work with google assistant

--- google-assistant/src/listen.sh
+++ google-assistant/src/listen.sh
@@ -1,6 +1,6 @@
 
 echo "Starting assistant..."
-googlesamples-assistant-hotword --project_id sarah-f0a98 --device_model_id my-model
+googlesamples-assistant-hotword --project_id sarah-f0a98 --device_model_id docker
 
 echo "Cannot start... Starting SSH"
 /usr/sbin/sshd -D
