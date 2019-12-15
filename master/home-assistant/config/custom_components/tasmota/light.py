296c36a7656c14e771de0a74a8e932ef55ee3b9c
Tomislav Zorcec <zorcec@gmail.com>
2019-12-15 14:01:12 +0100

    Extending auto-config, adding basic multipress suport, fixing touch switch bugs

--- master/home-assistant/config/custom_components/tasmota/light.py
+++ master/home-assistant/config/custom_components/tasmota/light.py
@@ -3,6 +3,7 @@ import logging
 import ptvsd
 import asyncio
 
+import homeassistant.components.mqtt as mqtt
 import homeassistant.helpers.config_validation as cv
 
 from homeassistant.components.mqtt.light.schema_basic import ( 
@@ -12,6 +13,7 @@ from homeassistant.components.mqtt.light.schema_basic import (
     CONF_ON_COMMAND_TYPE,
     CONF_BRIGHTNESS_COMMAND_TOPIC,
     CONF_BRIGHTNESS_STATE_TOPIC,
+    CONF_BRIGHTNESS_VALUE_TEMPLATE,
     CONF_COLOR_TEMP_COMMAND_TOPIC,
     CONF_COLOR_TEMP_STATE_TOPIC,
     CONF_WHITE_VALUE_STATE_TOPIC,
@@ -59,6 +61,16 @@ from ..sarah import (
 )
 
 _TYPE_COMMON = "common"
+_CONF_AUTO_CONFIG = "auto_config"
+
+_CONF_MULTIPRESS = {
+    "StateText3": "DOUBLE",
+    "ButtonTopic": "{internal_id}/multipress",
+    "SetOption1": 1,          # Multipress support
+    "SetOption11": 1,         # Multipress support
+    "SetOption13": 0,         # Will not react instantly
+    "SetOption32": 10         # HOLD after 1s
+}
 
 _TYPES = {
     _TYPE_COMMON: {
@@ -72,21 +84,49 @@ _TYPES = {
         CONF_PAYLOAD_OFF: "OFF",
         CONF_PAYLOAD_AVAILABLE: "Online",
         CONF_PAYLOAD_NOT_AVAILABLE: "Offline",
-        CONF_ON_COMMAND_TYPE: DEFAULT_ON_COMMAND_TYPE
+        CONF_ON_COMMAND_TYPE: DEFAULT_ON_COMMAND_TYPE,
+        _CONF_AUTO_CONFIG: {
+            "PowerOnState": 0,
+            "LedState": 0,
+            "Sleep": 0,
+            "SetOption31": 1           # Disable status LED blinking during Wi-Fi and MQTT connection problems. (1 is ON )
+        }
     },
-    "led_controller": {
+    "led_controller_rgb": {
         CONF_BRIGHTNESS_SCALE: 100,
         CONF_BRIGHTNESS_COMMAND_TOPIC: "cmnd/{internal_id}/DIMMER",
         CONF_RGB_COMMAND_TOPIC: "cmnd/{internal_id}/Color",
-        CONF_COLOR_TEMP_COMMAND_TOPIC: "cmnd/{internal_id}/CT",
+        _CONF_AUTO_CONFIG: {
+            "PwmFrequency": 1760
+        }
+    },
+    "led_controller_rgbw": {
+        CONF_BRIGHTNESS_SCALE: 100,
+        CONF_BRIGHTNESS_COMMAND_TOPIC: "cmnd/{internal_id}/DIMMER",
+        CONF_RGB_COMMAND_TOPIC: "cmnd/{internal_id}/Color",
         CONF_WHITE_VALUE_COMMAND_TOPIC: "cmnd/{internal_id}/Channel4" ,
-        CONF_WHITE_VALUE_SCALE: 100
+        CONF_WHITE_VALUE_SCALE: 100,
+        _CONF_AUTO_CONFIG: {
+            "PwmFrequency": 1760
+        }
     },
     "dimmer": {
         CONF_BRIGHTNESS_SCALE: 100,
-        CONF_BRIGHTNESS_COMMAND_TOPIC: "cmnd/{internal_id}/DIMMER"
+        CONF_BRIGHTNESS_COMMAND_TOPIC: "cmnd/{internal_id}/DIMMER",
+        _CONF_AUTO_CONFIG: {}
     },
-    "switch": {}
+    "touch": {
+        _CONF_AUTO_CONFIG: {
+            "SetOption13": 1,         # Will react instantly
+            "SetOption32": 10         # HOLD after 1s
+        }
+    },
+    "switch": {
+        _CONF_AUTO_CONFIG: {
+            "SetOption13": 1,
+            "SetOption15": 0           # 0 is basic PWM control, no dimmer and color
+        }
+    }
 }
 
 ptvsd.enable_attach()
@@ -113,11 +153,15 @@ class TasmotaLight(MqttLight):
     def __init__(self, hass, config, config_entry, discovery_hash):
         """Initializes a Tasmota light."""
         self._internal_id = config.get(CONF_INTERNAL_ID, None)
-        self.hass = hass
+        self._hass = hass
         self._loop = asyncio.new_event_loop()
+        self._config = self._get_config(config)
         _LOGGER.info("Initializing %s" % self._internal_id)
-        MqttLight.__init__(self, self._get_config(config), config_entry, discovery_hash)
+        MqttLight.__init__(self, self._config, config_entry, discovery_hash)
         asyncio.set_event_loop(self._loop)
+
+        self.refresh()
+        self.configureDevice()
 
         if "motion_iterval" in config:
             self.motionTimer = motion.Timer(hass, _LOGGER, config, self)
@@ -126,18 +170,52 @@ class TasmotaLight(MqttLight):
         self.set_config(config, _TYPES.get(_TYPE_COMMON))
         if CONF_TYPE in config and config.get(CONF_TYPE) in _TYPES:
             self.set_config(config, _TYPES.get(config.get(CONF_TYPE)))
-
         return config
 
     def set_config(self, config, data):
+        _data = self._prepareConfigData(data)
+        if "multipress" in config:
+            _data[_CONF_AUTO_CONFIG] = {
+                **_data[_CONF_AUTO_CONFIG],
+                **self._prepareConfigData(_CONF_MULTIPRESS)
+            }
+        for key in _data:
+            _value = _data.get(key)
+            if isinstance(_value, dict):
+                config[key] = {
+                    **config.get(key, {}),
+                    **_value
+                }
+            else:
+                config.setdefault(key, _value)
+    
+    def _prepareConfigData(self, data):
+        _data = {}
         _environment_values = {
             "internal_id": self._internal_id
         }
         for key in data:
-            _value = data.get(key)
-            if isinstance(_value, str):
-                _value = _value.format(**_environment_values)
-            config.setdefault(key, _value)
+            _computedValue = data.get(key)
+            if isinstance(_computedValue, str):
+                _computedValue = _computedValue.format(**_environment_values)
+            elif isinstance(_computedValue, dict):
+                _computedValue = self._prepareConfigData(_computedValue)
+            _data.setdefault(key, _computedValue)
+        return _data
+
+    def refresh(self):
+        _request_topics = [
+            self._config.get(CONF_COMMAND_TOPIC)
+        ]
+        for _request_topic in _request_topics:
+            mqtt.async_publish(self._hass, _request_topic, "")
+
+    def configureDevice(self):
+        _auto_config = self._config.get(_CONF_AUTO_CONFIG)
+        for _conf_topic in _auto_config:
+            _conf_value = _auto_config.get(_conf_topic)
+            _conf_topic = "cmnd/%s/%s" % (self._internal_id, _conf_topic)
+            mqtt.async_publish(self._hass, _conf_topic, _conf_value)
 
     def turn_on(self):
         self._loop.run_until_complete(self.async_turn_on())
