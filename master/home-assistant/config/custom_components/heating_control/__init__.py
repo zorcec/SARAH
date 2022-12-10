c8d3f8a8c6a54272998a212e2bad3139bb9a06cd
Tomislav <zorcec@gmail.com>
2022-12-10 13:34:24 +0000

    Improve heating (testing without valves)

--- master/home-assistant/config/custom_components/heating_control/__init__.py
+++ master/home-assistant/config/custom_components/heating_control/__init__.py
@@ -2,6 +2,7 @@ import logging
 import json
 import threading
 import pytz
+import time
 
 from datetime import datetime, timedelta
 from homeassistant import config_entries, core
@@ -10,14 +11,15 @@ from homeassistant.const import STATE_OFF, STATE_ON
 
 DOMAIN = "heating_control"
 CONF_HEATING_CYCLE = "heating_cycle"
-CONF_HEAT_REQUEST_NAME = "request_name"
-CONF_HEAT_REQUEST_STATE = "request_state"
 
 STATE_HEATING_REQUESTS = "heating_requests"
+
 
 _LOGGER = logging.getLogger(__name__)
 _VENT_OPEN_TIME = 5 # TODO Should be corrected later
 _VENT_ENTITIES = ["switch.tasmota_dc4f2255da78_switch_relay_1"]
+_PUMP_ENTITY_ID = "switch.tasmota"
+_HEAT_CYCLE_OVERRIDE = 600 # if not specified, the real config is taken
 
 _UTC = pytz.UTC
 
@@ -27,7 +29,7 @@ _UTC = pytz.UTC
 
 async def async_setup_entry(hass: core.HomeAssistant, entry: config_entries.ConfigEntry) -> bool:
     _LOGGER.info("Heating control is initialized")
-    heating_cycle(hass, entry.data[CONF_HEATING_CYCLE])
+    heating_cycle(hass, _HEAT_CYCLE_OVERRIDE or entry.data[CONF_HEATING_CYCLE])
     return True
 
 async def async_unload_entry(hass: core.HomeAssistant, entry: config_entries.ConfigEntry) -> bool:
@@ -39,15 +41,35 @@ def heating_cycle(hass, heating_cycle_time, heatingUp = True):
     threading.Timer(heating_cycle_time, heating_cycle, [hass, heating_cycle_time, not heatingUp]).start()
 
     if heatingUp and should_heat(hass):
-        heatingUpCycle()
+        heatingUpCycle(hass)
     else:
-        waitingCycle()
+        waitingCycle(hass)
 
-def heatingUpCycle():
-    _LOGGER.info("Turning pump on")
+def heatingUpCycle(hass):
+    _pump_state = hass.states.get(_PUMP_ENTITY_ID)
+    if _pump_state:
+        if _pump_state.state == STATE_OFF:
+            _LOGGER.info("Turning pump on")
+            hass.services.call("switch", "turn_on", {
+                "entity_id": _PUMP_ENTITY_ID
+            })
+        else:
+            _LOGGER.info("Pump is already on, ignoring")
+    else:
+        _LOGGER.info("Pump state is not known, ignoring")
 
-def waitingCycle():
-    _LOGGER.info("Turning pump off and waiting")
+def waitingCycle(hass):
+    _pump_state = hass.states.get(_PUMP_ENTITY_ID)
+    if _pump_state:
+        if _pump_state.state == STATE_ON:
+            _LOGGER.info("Turning pump off and waiting")
+            hass.services.call("switch", "turn_off", {
+                "entity_id": _PUMP_ENTITY_ID
+            })
+        else:
+            _LOGGER.info("Pump is already off, ignoring")
+    else:
+        _LOGGER.info("Pump state is not known, ignoring")
 
 def should_heat(hass):
     """ Checks if we should start the heating pump; Vent open time is considered in it"""
@@ -62,3 +84,4 @@ def should_heat(hass):
                 return True
 
     return False
+
