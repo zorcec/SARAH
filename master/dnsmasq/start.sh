d484bfa1b39a9893b8e99559f6f686c2a3194798
Tomislav Zorcec <zorcec@gmail.com>
2023-11-24 22:09:26 +0100

    Update HA and cleanups

--- master/dnsmasq/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#echo "--- Starting arp scan ---"
-#/sbin/ifconfig -a
-#touch /etc/hosts.home
-#arp-scan -l --interface eth0 -m /etc/hosts.home | head -n-3 | tail -n+3 | cut -f1,3-
-echo "--- Starting dnsmasq ---"
-webproc -u user --config /etc/config/dnsmasq.conf -- dnsmasq --no-daemon
